727e15432389d0e82587d3fa8a0b4af2d751519b
Teodor Vecerdi <teodor.vecerdi@gmail.com>
2020-08-31 16:37:36 +0300

    Add player movement prototype

---
+++ Assets/Scripts/TurtleController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurtleController : MonoBehaviour {
+    public float Speed = 10f;
+    void Start() {
+        
+    }
+
+    void Update() {
+        var position = transform.position;
+        position += Vector3.forward * (Speed * Time.deltaTime);
+        transform.position = position;
+    }
+}
---
+++ Assets/Scripts/TurtleWobbleAnimation.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurtleWobbleAnimation : MonoBehaviour {
+    [Header("Settings")]
+    public float MaxY = 0.25f;
+    public float Speed = 0.25f;
+    public float RotationPeak = 15f;
+    public AnimationCurve Motion;
+
+    [Header("Current Stats")]
+    public int Direction = 1;
+    public float CurrentTime = 0f;
+
+    private void Start() {
+        transform.localPosition = new Vector3(0, -Direction * MaxY, 0);
+    }
+
+    private void Update() {
+
+        var currentPosition = transform.localPosition;
+        var currentY = Motion.Evaluate(CurrentTime) * MaxY * 2 - MaxY;
+        currentPosition.y = currentY;
+        transform.localPosition = currentPosition;
+        
+        var currentRot = (-Motion.Evaluate(CurrentTime + .5f) * RotationPeak * 2 + RotationPeak) * Direction;
+        transform.localRotation = Quaternion.Euler(currentRot, 0f, 0f);
+
+        CurrentTime += Direction * Speed * Time.deltaTime;
+        if (CurrentTime >= 1f) {
+            CurrentTime = 1f;
+            Direction *= -1;
+        } else if (CurrentTime <= 0f) {
+            CurrentTime = 0f;
+            Direction *= -1;
+        }
+
+    }
+}
