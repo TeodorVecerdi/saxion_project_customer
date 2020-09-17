4763c336c1b686ffee1181a1f03c54c992887419
Teodor Vecerdi <teodor.vecerdi@gmail.com>
2020-09-17 18:42:47 +0300

    Add build version 0.9.9d

--- Assets/Scripts/Environment/EnvironmentController.cs
+++ Assets/Scripts/Environment/EnvironmentController.cs
@@ -3,11 +3,17 @@ using UnityEngine;
 
     public class EnvironmentController : MonoBehaviour {
         public float ResetDistance = 5f;
+        public Material CausticsMaterial;
+        private static readonly int Distance = Shader.PropertyToID("_Distance");
 
         private void Update() {
+            var currentDistance = CausticsMaterial.GetFloat(Distance);
             if (transform.position.z <= -ResetDistance) {
                 transform.position += new Vector3(0f, 0f, ResetDistance);
+                currentDistance -= ResetDistance;
             }
+            
+            CausticsMaterial.SetFloat("_Distance", currentDistance + TurtleState.Instance.CurrentSpeed * GameTime.DeltaTime);
             transform.position -= new Vector3(0f, 0f, TurtleState.Instance.CurrentSpeed * GameTime.DeltaTime);
         }
     }
